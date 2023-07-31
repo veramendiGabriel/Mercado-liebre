83312c32e60b22961e58937a202c94b7ac4da569
veramendiGabriel <veramendig97@gmail.com>
2023-07-31 16:49:40 -0300

    commit de mercado liebre

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express()
+const PORT = process.env.PORT || 3030
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.listen(PORT,() => {
+    console.log('Escuchando en el puerto ' + PORT);
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
